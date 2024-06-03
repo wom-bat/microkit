b2f004efb50e174898e370fb89bbbb7e4a8dc1c8
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-06-03 12:41:15 +1000

    Allow small BSS and data sections for RISC-V
    The small data and BSS sections can be accessed with a 12-bit signed offset
    from the global pointer.
    
    This patch:
      -- Includes the small data at the end of the DATA segment
      -- Include small BSS at the start of the BSS segment
      -- Sets the global pointer symbol to 2k from the start of small data
      -- Sets the global pointer register to the value of the global
         pointer symbol
    
    Signed-off-by: Peter Chubb <peter.chubb@unsw.edu.au>

--- libmicrokit/src/riscv/crt0.s
+++ libmicrokit/src/riscv/crt0.s
@@ -5,6 +5,11 @@
 .global _start;
 .type _start, %function;
 _start:
+.option push
+.option norelax
+1:  auipc gp, %pcrel_hi(__global_pointer$)
+    addi  gp, gp, %pcrel_lo(1b)
+.option pop
     la s1, (_stack + 0xff0)
     mv sp, s1
     j main
