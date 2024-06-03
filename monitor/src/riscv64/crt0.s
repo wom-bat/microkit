baa5d2a9db9e936d91106f315022e9e3354fd2a3
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-06-03 14:13:09 +1000

    Fix RISC-V loader and monitor to use GP
    Inlcude sbss and sdata in the appropriate segments; and set the
    global pointer correctly in crt0.s
    
    Signed-off-by: Peter Chubb <peter.chubb@unsw.edu.au>

--- monitor/src/riscv64/crt0.s
+++ monitor/src/riscv64/crt0.s
@@ -1,10 +1,16 @@
 .extern main
+.extern __global_pointer$
 
 .section ".text.start"
 
 .global _start;
 .type _start, %function;
 _start:
+.option push
+.option norelax
+1:  auipc gp, %pcrel_hi(__global_pointer$)
+    addi  gp, gp, %pcrel_lo(1b)
+.option pop
     la s1, (_stack + 0xff0)
     mv sp, s1
     j main
